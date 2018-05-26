bcd0fe87387c134282c635e9118db541d1c133a2
Nick Yang <nick.yang3721@gmail.com>
2018-05-26 13:46:20 -0400

    Added the List Carosel

--- src/List.js
+++ src/List.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import "./List.css";
  
 class List extends Component {
   render() {
     return (
-      <div> 
-      List
-      </div>
+    	<div className = "box">
+      <form>
+        <input type="radio" name="fancy" autofocus value="Food" id="food" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" value="Entertainment" id="entertainment" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" value="Medication" id="medication" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" value="Conversation" id="conversation" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" value="Input" id="input" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+        <label for="food">Food</label>
+        <label for="entertainment">Entertainment</label>
+        <label for="medication">Medication</label>
+        <label for="conversation">Conversation</label>
+        <div className="keys">Use left and right motions to navigate</div>
+    </form>
+    </div>
     );
   }
 }
