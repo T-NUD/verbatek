5a313c338dd1d4437f841304f88fa82c6e13cb41
Nick Yang <nick.yang3721@gmail.com>
2018-05-26 16:56:02 -0400

    FInal Changes
    Updated the List.css styling, as

--- src/List.js
+++ src/List.js
@@ -1,35 +1,30 @@
 import React, { Component } from "react";
 import "./List.css";
 
-function onKeyDown(id, event) {
-  var x = event.key;
-  //13 is enter
-  if (x === 13){
-    if (id === "food"){
-      //
-    }
-  }
-}
-
-function onKeyUp() { }
 
 class List extends Component {
   render() {
     return (
-    	<div className = "container">
       <form>
-        <input type="radio" name="fancy" autofocus value="Food" id="food" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" autofocus value="Food" id="food" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
         <input type="radio" name="fancy" value="Entertainment" id="entertainment" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
-        <input type="radio" name="fancy" value="Medication" id="medication" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
-        <input type="radio" name="fancy" value="Conversation" id="conversation" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
-        <input type="radio" name="fancy" value="Input" id="input" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+
+
+            <input type="radio" name="fancy" value="Medication" id="medication" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
+
+
+            <input type="radio" name="fancy" value="Conversation" id="conversation" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
+
+   
+            <input type="radio" name="fancy" value="Input" id="input" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+      
         <label for="food">Food</label>
         <label for="entertainment">Entertainment</label>
         <label for="medication">Medication</label>
         <label for="conversation">Conversation</label>
-        <div className="keys">Use left and right motions to navigate</div>
+        <div class="keys">Use left and right motions to navigate</div>
+        <div class="map"><strong>Food | Entertainment | Medication | Conversation</strong></div>
     </form>
-    </div>
     );
   }
 }
---
+++ src/Object.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Object.css";
+ 
+class Thing extends Component {
+  render() {
+    return (
+    	<p> HELLO </p>
+    );
+  }
+}
+ 
+export default Thing;
