828aac14874c8e2906f02d63c8a59cdf0f5740e8
Rohil Gupta <rohilvg@gmail.com>
2018-05-26 14:44:39 -0400

    added test for google.ca

--- src/List.js
+++ src/List.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import "./List.css";
- 
+
+function onKeyDown(id, event) {
+  var x = event.key;
+  //13 is enter
+  if (x == 13){
+    if (id == "food"){
+      
+    }
+  }
+}
+function onKeyUp() { }
+
 class List extends Component {
   render() {
     return (
     	<div className = "container">
       <form>
-        <input type="radio" name="fancy" autofocus value="Food" id="food" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
-        <input type="radio" name="fancy" value="Entertainment" id="entertainment" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
-        <input type="radio" name="fancy" value="Medication" id="medication" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
-        <input type="radio" name="fancy" value="Conversation" id="conversation" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
+        <a href="www.google.ca"> <input type="radio" name="fancy" autofocus value="Food" id="food" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
+        </a>
+        <input type="radio" name="fancy" value="Entertainment" id="entertainment" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" value="Medication" id="medication" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
+        <input type="radio" name="fancy" value="Conversation" id="conversation" onkeydown="onKeyDown(id, event)" onkeyup="onKeyUp()"/>
         <input type="radio" name="fancy" value="Input" id="input" onkeydown="onKeyDown(event)" onkeyup="onKeyUp()"/>
         <label for="food">Food</label>
         <label for="entertainment">Entertainment</label>
@@ -21,5 +33,5 @@ class List extends Component {
     );
   }
 }
- 
+
 export default List;
